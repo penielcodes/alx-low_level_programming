1d56937132adefe1b501311b4c6d8ba7694cec96
penielcodes <ckaima15peniel@gmail.com>
2023-02-17 16:37:22 -0800

    commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int $CFILE;
+  $CFILE = main.c;
+
+  printf ("Hello World!\n")
+    return (0);
+}
